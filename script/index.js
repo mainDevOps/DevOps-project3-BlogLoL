ce9455354f13467e781227eafea6fc77d8d907ee
mainDevOps <main.devopsar@gmail.com>
2022-03-27 20:11:08 -0300

    revision navbar

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-function burgerButton() {
+const burgerButton = () => {
     const burgerBtn = document.getElementById("burger-button").classList.toggle("change")
     const showMenu = document.getElementById("burger-menu").classList.toggle("show-menu")
 }
