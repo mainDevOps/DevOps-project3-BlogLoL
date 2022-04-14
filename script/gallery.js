30c852a2317b25e78d7ea69a6925f87f84ca635e
mainDevOps <main.devopsar@gmail.com>
2022-04-13 23:11:14 -0300

    adding json from gallery

--- script/gallery.js
+++ script/gallery.js
@@ -1,3 +1,7 @@
+async function getChampions(){
+    const response = await fetch('http://127.0.0.1:5500/devops/DevOps-project3-BlogLoL/json/gallery.json')
+}
+
 const containerBox = document.querySelector('.container-box-pictures')
 
 window.addEventListener('load', () => {
--- script/index.js
+++ script/index.js
@@ -1,11 +1,12 @@
-const innerHeader = (document.getElementById("header").innerHTML = `
+const innerHeader = document.getElementById("header").innerHTML = (
+    `
     <nav id="container-nav"></nav>
     <nav id="burger-menu"></nav>
-    `);
+    `
+)
 
-const innerNavContainer = (document.getElementById(
-  "container-nav"
-).innerHTML = `
+const innerNavContainer = document.getElementById("container-nav").innerHTML = (
+    `
     <a href="index.html"><img src="./src/Logo.png" class="logo"></a>
     <div id="burger-button">
         <div class="bar1"></div>
@@ -20,58 +21,63 @@ const innerNavContainer = (document.getElementById(
             <li><a href="login.html">LOGIN</a></li>
         </ul>
     </div>
-    `);
+    `
+)
 
-const innerNavMenu = (document.getElementById("burger-menu").innerHTML = `
+const innerNavMenu = document.getElementById("burger-menu").innerHTML = (
+    `
     <ul>
     <li><a href="gallery.html">GALLERY</a></li>
     <li><a href="about.html">ABOUT US</a></li>
     <li><a href="contact.html">CONTACT</a></li>
     <li><a href="login.html">LOGIN</a></li>
     </ul>
-    `);
+    `
+)
 
-const innerBurgerMenu = document
-  .getElementById("burger-button")
-  .addEventListener("click", () => {
-    const burgerBtn = document
-      .getElementById("burger-button")
-      .classList.toggle("change");
-    const showMenu = document
-      .getElementById("burger-menu")
-      .classList.toggle("show-menu");
-  });
-
-
-<<<<<<< HEAD
-
-=======
-form.addEventListener("submit", (e) => {
-  addEventListener.preventDefault();
-  let warnings = "";
-  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  parrafo.innerHTML = "";
-  if (nombre.value.length < 6) {
-    aler("short name");
-  }
-
-  console.log(regexEmail);
-  if (!regexEmail.test(email.value)) {
-    warnings += "invalid email <br>";
-    entrar = true;
-  }
-  if (pass.value.length < 8) {
-    warnings += "invalid password <br>";
-    entrar = true;
-  }
-  if (repass.value.length == pass.value.length) {
-    warnings += "invalid password <br>";
-    entrar = true;
-  }
-  if (entrar) {
-    parrafo.innerHTML = warnings;
-  } else {
-    alert("success");
-  }
+const innerBurgerMenu = document.getElementById("burger-button").addEventListener('click', () => {
+    const burgerBtn = document.getElementById("burger-button").classList.toggle("change")
+    const showMenu = document.getElementById("burger-menu").classList.toggle("show-menu")
 })
->>>>>>> a7b4c7f8aa372b8a0e1336d755289f64aaa03513
+
+
+const email = document.getElementById("email");
+const pass = document.getElementById("mypassword");
+const repass = document.getElementById("REppassword");
+const birth = document.getElementById("dbirthday");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e=>{
+    addEventListener.preventDefault();
+    let warnings ="";
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    parrafo.innerHTML = "";
+    if(nombre.value.length <6){
+        aler("short name");
+    }
+    
+    console.log(regexEmail)
+    if(!regexEmail.test(email.value)){
+        warnings += 'invalid email <br>';
+        entrar = true;
+
+    }
+    if(pass.value.length <8){
+        warnings += 'invalid password <br>';
+        entrar = true;
+    }
+    if(repass.value.length == pass.value.length){
+        warnings += 'invalid password <br>';
+        entrar = true;
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings;
+    }
+    else{
+        alert("success");
+    }
+
+})
+
+/* emailjs.send(service_e53plim, template_zuxutbl, templateParams, user_Y41fnJgv1nj4ow0DJZfmZ); */
